fa48258c3460738db5c163a50b7949bb92a2a7ab
Yhya <yhyamobile57@gmail.com>
2024-03-23 06:01:01 +0200

    refactor the repl a little bit and add the ability of multi-line when using '[' or '('

--- crates/core/src/repl.rs
+++ crates/core/src/repl.rs
@@ -5,7 +5,7 @@ use syphon_bytecode::value::Value;
 use syphon_gc::{GarbageCollector, TraceFormatter};
 use syphon_vm::VirtualMachine;
 
-use std::io::{stdin, stdout, BufRead, BufReader, Write};
+use std::io::{stdin, stdout, BufRead, BufReader, Stdin, Write};
 
 pub fn start() {
     let mut reader = BufReader::new(stdin());
@@ -13,45 +13,23 @@ pub fn start() {
     let mut gc = GarbageCollector::new();
 
     let mut vm = VirtualMachine::new(&mut gc);
-
     vm.init_globals();
-    loop {
-        
-        let mut input = String::new();
-            print!(">> ");
-        
 
+    loop {
+        let mut input = String::new();
+
+        print!(">> ");
         stdout().flush().unwrap();
 
         reader.read_line(&mut input).unwrap();
 
-        
-        input = input.trim_end_matches('\n').to_string();
-
-        
         if input.is_empty() {
-            continue;
-        } 
-        
-        if input == "\x03" {
             break;
         }
 
-        let mut opened = input.chars().filter(|&c| c == '{').count();
-        let mut closed = input.chars().filter(|&c| c == '}').count();
-
-        while opened > closed {
-            print!("..{}", "  ".repeat(opened));
-            stdout().flush().unwrap();
-
-            let mut input_nest = String::new();
-            reader.read_line(&mut input_nest).unwrap();
-            input_nest = input_nest.trim_end_matches('\n').to_string();
-            input += &input_nest;
-            closed += input_nest.chars().filter(|&c| c == '}').count();
-            opened += input_nest.chars().filter(|&c| c == '{').count();
-
-        }
+        let _ = !handle_multi_line(&mut reader, &mut input, '{', '}')
+            && !handle_multi_line(&mut reader, &mut input, '[', ']')
+            && !handle_multi_line(&mut reader, &mut input, '(', ')');
 
         let Some(value) = runner::run_repl("<stdin>", input, &mut vm) else {
             continue;
@@ -59,7 +37,38 @@ pub fn start() {
 
         match value {
             Value::None => (),
+
             _ => println!("{}", TraceFormatter::new(value, vm.gc)),
         };
     }
+}
+
+fn handle_multi_line(
+    reader: &mut BufReader<Stdin>,
+    input: &mut String,
+    open_character: char,
+    close_character: char,
+) -> bool {
+    let mut open = input.chars().filter(|&c| c == open_character).count();
+
+    let mut close = input.chars().filter(|&c| c == close_character).count();
+
+    let used_open = open > close;
+
+    while open > close {
+        print!(".. ");
+        stdout().flush().unwrap();
+
+        let mut input_nest = String::new();
+
+        reader.read_line(&mut input_nest).unwrap();
+
+        *input += input_nest.as_str();
+
+        open += input_nest.chars().filter(|&c| c == open_character).count();
+
+        close += input_nest.chars().filter(|&c| c == close_character).count();
+    }
+
+    used_open
 }

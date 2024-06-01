a4ba506249d2bdf664b5a9d1620d182504d7b11b
Yhya <yhyamobile57@gmail.com>
2024-06-01 19:59:17 +0300

    remove unit tests

--- build.zig
+++ build.zig
@@ -24,15 +24,4 @@ pub fn build(b: *std.Build) void {
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
-
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
-
-    const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
 }

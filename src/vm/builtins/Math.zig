02f772c914df2d57b7c86d6c2898678b31b53761
Yhya <yhyamobile57@gmail.com>
2024-06-13 09:50:00 +0300

    more math constants

--- src/vm/builtins/Math.zig
+++ src/vm/builtins/Math.zig
@@ -6,6 +6,9 @@ pub fn getExports(gpa: std.mem.Allocator) std.mem.Allocator.Error!VirtualMachine
     var exports = std.StringHashMap(VirtualMachine.Code.Value).init(gpa);
 
     try exports.put("pi", .{ .float = std.math.pi });
+    try exports.put("e", .{ .float = std.math.e });
+    try exports.put("phi", .{ .float = std.math.phi });
+    try exports.put("tau", .{ .float = std.math.tau });
 
     return VirtualMachine.Code.Value.Object.Map.fromStringHashMap(gpa, exports);
 }

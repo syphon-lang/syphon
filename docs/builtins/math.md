4e7ab4a26edd3740eadb3b2c28eb1345a660bed6
Yhya <yhyamobile57@gmail.com>
2024-06-20 14:01:38 +0300

    add "sin" and "cos" native functions

--- docs/builtins/math.md
+++ docs/builtins/math.md
@@ -19,9 +19,27 @@ math = import("math")
 
 - sqrt
 
-Gives you an approximation of the square root of a number
+Gives you the square root of a number
 
 ```
 math.sqrt(4) # 2.0
 math.sqrt(65) # 8.06225774829855
+```
+
+- sin
+
+Gives you the sine of an angle measured in radians
+
+```
+math.sin(180) # -0.8011526357338304
+math.sin(90) # 0.8939966636005579
+```
+
+- cos
+
+Gives you the cosine of an angle measured in radians
+
+```
+math.cos(180) # -0.5984600690578581
+math.cos(90) # -0.4480736161291702
 ```
--- src/vm/builtins/Math.zig
+++ src/vm/builtins/Math.zig
@@ -10,7 +10,10 @@ pub fn getExports(vm: *VirtualMachine) std.mem.Allocator.Error!Code.Value {
     try exports.put("e", .{ .float = std.math.e });
     try exports.put("phi", .{ .float = std.math.phi });
     try exports.put("tau", .{ .float = std.math.tau });
+
     try exports.put("sqrt", Code.Value.Object.NativeFunction.init(1, &sqrt));
+    try exports.put("sin", Code.Value.Object.NativeFunction.init(1, &sin));
+    try exports.put("cos", Code.Value.Object.NativeFunction.init(1, &cos));
 
     return Code.Value.Object.Map.fromStringHashMap(vm.gpa, exports);
 }
@@ -21,4 +24,20 @@ fn sqrt(vm: *VirtualMachine, arguments: []const Code.Value) Code.Value {
     const value = Type.toFloat(vm, arguments);
 
     return Code.Value{ .float = @sqrt(value.float) };
+}
+
+fn sin(vm: *VirtualMachine, arguments: []const Code.Value) Code.Value {
+    const Type = @import("Type.zig");
+
+    const value = Type.toFloat(vm, arguments);
+
+    return Code.Value{ .float = @sin(value.float) };
+}
+
+fn cos(vm: *VirtualMachine, arguments: []const Code.Value) Code.Value {
+    const Type = @import("Type.zig");
+
+    const value = Type.toFloat(vm, arguments);
+
+    return Code.Value{ .float = @cos(value.float) };
 }

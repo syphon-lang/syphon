27424255c0ef5f5973b6224ff6c51baee5fbea95
Yhya <yhyamobile57@gmail.com>
2024-07-03 15:54:19 +0300

    inline functions in the `StringHashMapRecorder` struct

--- src/vm/string_hash_map_recorder.zig
+++ src/vm/string_hash_map_recorder.zig
@@ -39,7 +39,7 @@ pub fn StringHashMapRecorder(comptime V: type) type {
             return null;
         }
 
-        pub fn getFromLastSnapshot(self: Self, key: K) ?V {
+        pub inline fn getFromLastSnapshot(self: Self, key: K) ?V {
             if (self.snapshots.items.len == 0) {
                 return null;
             }
@@ -47,7 +47,7 @@ pub fn StringHashMapRecorder(comptime V: type) type {
             return self.snapshots.getLast().get(key);
         }
 
-        pub fn put(self: *Self, key: K, value: V) std.mem.Allocator.Error!void {
+        pub inline fn put(self: *Self, key: K, value: V) std.mem.Allocator.Error!void {
             if (self.snapshots.items.len == 0) {
                 self.newSnapshot();
             }

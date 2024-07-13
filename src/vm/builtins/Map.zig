2050d6da00d6028e5d1ccbd2ef4a3484fd664a86
Yhya <yhyamobile57@gmail.com>
2024-07-13 10:53:57 +0300

    make `map_foreach` function

--- src/vm/builtins/Map.zig
+++ src/vm/builtins/Map.zig
@@ -8,6 +8,7 @@ pub fn addGlobals(vm: *VirtualMachine) std.mem.Allocator.Error!void {
     try vm.globals.put(try Atom.new("map_keys"), Code.Value.Object.NativeFunction.init(1, &map_keys));
     try vm.globals.put(try Atom.new("map_from_keys"), Code.Value.Object.NativeFunction.init(1, &map_from_keys));
     try vm.globals.put(try Atom.new("map_values"), Code.Value.Object.NativeFunction.init(1, &map_values));
+    try vm.globals.put(try Atom.new("map_foreach"), Code.Value.Object.NativeFunction.init(2, &map_foreach));
 }
 
 fn map_keys(vm: *VirtualMachine, arguments: []const Code.Value) Code.Value {
@@ -84,4 +85,39 @@ fn map_values(vm: *VirtualMachine, arguments: []const Code.Value) Code.Value {
             return Code.Value{ .none = {} };
         },
     };
+}
+
+fn map_foreach(vm: *VirtualMachine, arguments: []const Code.Value) Code.Value {
+    if (!(arguments[0] == .object and arguments[0].object == .map)) {
+        return Code.Value{ .none = {} };
+    }
+
+    if (!(arguments[1] == .object and arguments[1].object == .function)) {
+        return Code.Value{ .none = {} };
+    }
+
+    const map = arguments[0].object.map;
+    const callback = arguments[1].object.function;
+
+    var map_entry_iterator = map.inner.iterator();
+
+    while (map_entry_iterator.next()) |map_entry| {
+        vm.stack.append(map_entry.key_ptr.*) catch unreachable;
+        vm.stack.append(map_entry.value_ptr.*) catch unreachable;
+
+        const frame = &vm.frames.items[vm.frames.items.len - 1];
+
+        const previous_frames_start = vm.frames_start;
+        vm.frames_start = vm.frames.items.len;
+
+        vm.callUserFunction(callback, frame) catch unreachable;
+
+        vm.run() catch unreachable;
+
+        vm.frames_start = previous_frames_start;
+
+        _ = vm.stack.pop();
+    }
+
+    return Code.Value{ .none = {} };
 }

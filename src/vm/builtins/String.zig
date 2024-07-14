c087fcab4afa7aee618432b7fdde3be13a287f0d
The Darsh <129121284+its-darsh@users.noreply.github.com>
2024-07-14 11:19:36 +0300

    feat: add functions `string_upper` and `string_lower`

--- src/vm/builtins/String.zig
+++ src/vm/builtins/String.zig
@@ -6,6 +6,8 @@ const Atom = @import("../Atom.zig");
 
 pub fn addGlobals(vm: *VirtualMachine) std.mem.Allocator.Error!void {
     try vm.globals.put(try Atom.new("string_split"), Code.Value.Object.NativeFunction.init(2, &stringSplit));
+    try vm.globals.put(try Atom.new("string_upper"), Code.Value.Object.NativeFunction.init(1, &stringUpper));
+    try vm.globals.put(try Atom.new("string_lower"), Code.Value.Object.NativeFunction.init(1, &stringLower));
     try vm.globals.put(try Atom.new("ord"), Code.Value.Object.NativeFunction.init(1, &ord));
     try vm.globals.put(try Atom.new("chr"), Code.Value.Object.NativeFunction.init(1, &chr));
 }
@@ -41,6 +43,40 @@ fn stringSplit(vm: *VirtualMachine, arguments: []const Code.Value) Code.Value {
     }
 
     return Code.Value.Object.Array.fromStringSlices(vm.allocator, new_strings.items) catch Code.Value{ .none = {} };
+}
+
+fn stringUpper(vm: *VirtualMachine, arguments: []const Code.Value) Code.Value {
+    if (!(arguments[0] == .object and arguments[0].object == .string)) {
+        return Code.Value{ .none = {} };
+    }
+
+    const string = arguments[0].object.string;
+    var new_string = std.ArrayList(u8).init(vm.allocator);
+
+    for (0..string.content.len) |i| {
+        const char = string.content[i];
+        const new_char = std.ascii.toUpper(char);
+        new_string.append(new_char) catch new_string.append(new_char) catch continue;
+    }
+
+    return Code.Value{ .object = .{ .string = .{ .content = new_string.items } } };
+}
+
+fn stringLower(vm: *VirtualMachine, arguments: []const Code.Value) Code.Value {
+    if (!(arguments[0] == .object and arguments[0].object == .string)) {
+        return Code.Value{ .none = {} };
+    }
+
+    const string = arguments[0].object.string;
+    var new_string = std.ArrayList(u8).init(vm.allocator);
+
+    for (0..string.content.len) |i| {
+        const char = string.content[i];
+        const new_char = std.ascii.toLower(char);
+        new_string.append(new_char) catch new_string.append(new_char) catch continue;
+    }
+
+    return Code.Value{ .object = .{ .string = .{ .content = new_string.items } } };
 }
 
 fn ord(vm: *VirtualMachine, arguments: []const Code.Value) Code.Value {

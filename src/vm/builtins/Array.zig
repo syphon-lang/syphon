0dfc2f9ea34bbc610281e66c8836f949789f9af7
The Darsh <129121284+its-darsh@users.noreply.github.com>
2024-07-17 08:30:27 +0300

    feat: add functions `range` and `reverse` (#46)
    * feat: add functions `range` and `reverse`
    
    this commit also removes the function `array_reverse` in favor of the new function `reverse` which can handle both arrays and strings
    
    * docs: document `range` and `reverse`
    
    * feat: provide tests for `range` and `reverse`

--- src/vm/builtins/Array.zig
+++ src/vm/builtins/Array.zig
@@ -7,7 +7,6 @@ const Atom = @import("../Atom.zig");
 pub fn addGlobals(vm: *VirtualMachine) std.mem.Allocator.Error!void {
     try vm.globals.put(try Atom.new("array_push"), Code.Value.Object.NativeFunction.init(2, &arrayPush));
     try vm.globals.put(try Atom.new("array_pop"), Code.Value.Object.NativeFunction.init(1, &arrayPop));
-    try vm.globals.put(try Atom.new("array_reverse"), Code.Value.Object.NativeFunction.init(1, &arrayReverse));
 }
 
 fn arrayPush(vm: *VirtualMachine, arguments: []const Code.Value) Code.Value {
@@ -38,22 +37,4 @@ fn arrayPop(vm: *VirtualMachine, arguments: []const Code.Value) Code.Value {
     const array = arguments[0].object.array;
 
     return array.values.popOrNull() orelse Code.Value{ .none = {} };
-}
-
-fn arrayReverse(vm: *VirtualMachine, arguments: []const Code.Value) Code.Value {
-    if (!(arguments[0] == .object and arguments[0].object == .array)) {
-        return Code.Value{ .none = {} };
-    }
-
-    const array = arguments[0].object.array;
-
-    var new_array = std.ArrayList(Code.Value).initCapacity(vm.allocator, array.values.items.len) catch |err| switch (err) {
-        else => return Code.Value{ .none = {} },
-    };
-
-    for (0..array.values.items.len) |i| {
-        new_array.appendAssumeCapacity(array.values.items[array.values.items.len - 1 - i]);
-    }
-
-    return Code.Value.Object.Array.init(vm.allocator, new_array) catch Code.Value{ .none = {} };
 }
